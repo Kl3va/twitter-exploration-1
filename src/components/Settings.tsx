d0118cda75d0524c4375483285975bf94f311232
Ikwunze Kelvin <ikwunzekelvin@gmail.com>
2025-10-04 02:20:38 +0100

    fix: fix build errors

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { Wrench } from "lucide-react";
 
-type Props = {};
-
-const Settings = (props: Props) => {
+const Settings = () => {
   return (
     <div className="grid gap-4 py-8">
       <h1 className="text-2xl text-black font-bold">Settings</h1>
